c2c86f01915d879b7870af4977b2b6e36554c2a1
Savannah Carlin <savannahcarlin@Savannahs-MacBook-Pro.local>
2018-04-17 15:27:58 -0400

    fourth commit

--- app.js
+++ app.js
@@ -1,48 +1,51 @@
+function onReady(){
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', () => {
+    event.preventDefault();
+
+    // get the text
+    let title = newToDoText.value;
+
+    // create a new li
+    let newLi = document.createElement('li');
+
+    // create a new input
+    let checkbox = document.createElement('input');
+
+    checkbox.type = "checkbox";
+
+    // create a ability to delete item
+    let minusBtn = document.createElement('button');
+    minusBtn.innerHTML = '<span>Delete</span>';
+
+    // set the title
+    newLi.textContent = title;
+
+    // attach the checkbox to the li
+    newLi.appendChild(checkbox);
+
+    // attach the checkbox to the li
+    newLi.appendChild(minusBtn);
+
+    // attach the li to the ul
+    toDoList.appendChild(newLi);
+
+    // empty the input value
+    newToDoText.value = '';
+
+    // listening for minusBtn to be clicked and removing the item
+    minusBtn.addEventListener('click', function() {
+      newLi.parentNode.removeChild(newLi);
+    })
+
+    console.log(title);
+  });
+
+};
+
 window.onload = function() {
-  //alert("The window has loaded!");
-  onReady(); {
-    const addToDoForm = document.getElementById('addToDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
-
-    addToDoForm.addEventListener('submit', () => {
-      event.preventDefault();
-      // get the text
-      let title = newToDoText.value;
-
-      // create a new li
-      let newLi = document.createElement('li');
-
-      // create a new input
-      let checkbox = document.createElement('input');
-
-      //create delete button
-      let deleteButton = document.createElement('submit');
-
-      // set the input's type to checkbox
-      checkbox.type = "checkbox";
-
-      //set the input's type to submit
-      deleteButton.type = "input";
-
-      //set the input's text to delete
-      deleteButton.content = "Delete";
-
-      // set the title
-      newLi.textContent = title;
-
-      // attach the checkbox to the li
-      newLi.appendChild(checkbox);
-
-      //attach the delete button to the li
-      newLi.appendChild(deleteButton);
-
-      // attach the li to the ul
-      toDoList.appendChild(newLi);
-
-      //empty the input
-      newToDoText.value = '';
-
-    });
-  }
+  onReady();
 };
